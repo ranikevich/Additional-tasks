2873c13b5ad8703bbd24212e8425c85714683825
Rimma Anikevich <ranikevich@gmail.com>
2022-03-27 13:56:49 +0700

    Add a sort of number

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+let num = 266219;
+let mult;
+let temp;
 
+while (num) {
+    temp = num % 10;
+    console.log (temp);
+    mult *= temp;
+    console.log (typeof mult);
+    console.log (mult);
+    num = (num - temp) / 10;
+}
+
+let result = mult ** 3;
+
+console.log (typeof num);
+console.log (mult);
+console.log (result.substring(0,2));
